b9e319b28a014df1da8a7ce20006a0d9507e72fb
ParsaKhaz <khazapar7@gmail.com>
2024-12-04 21:40:46 -0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import nextra from 'nextra'
 import type { NextConfig } from 'next'
 
+const isProduction = process.env.NODE_ENV === 'production'
+const basePath = '/moondream-docs'
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -8,7 +11,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true // mandatory, otherwise won't export
-  }
+  },
+  basePath,
+  assetPrefix: isProduction ? basePath : ''
 }
 
 const withNextra = nextra({
